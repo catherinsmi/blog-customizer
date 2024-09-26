5484fe7cb3461e493cee8ecfabaeb7e0de48809e
catherinsmi <catherinsmi@yandex.ru>
2024-09-26 12:05:51 +0300

    feat: adds styles variator

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -1,20 +1,67 @@
 import { ArrowButton } from 'components/arrow-button';
 import { Button } from 'components/button';
-
+import { Text } from '../text';
+import { Select } from '../select';
+import { Separator } from '../separator';
+import { RadioGroup } from '../radio-group';
+import { useState } from 'react';
+import { clsx } from 'clsx';
 import styles from './ArticleParamsForm.module.scss';
+import {fontColors, fontFamilyOptions,backgroundColors, fontSizeOptions, defaultArticleState, contentWidthArr, OptionType, ArticleStateType} from 'src/constants/articleProps';
 
-export const ArticleParamsForm = () => {
-	return (
-		<>
-			<ArrowButton />
-			<aside className={styles.container}>
-				<form className={styles.form}>
-					<div className={styles.bottomContainer}>
-						<Button title='Сбросить' type='reset' />
-						<Button title='Применить' type='submit' />
-					</div>
-				</form>
-			</aside>
-		</>
-	);
+interface ArticleParamsFormProps {
+    setStyles: (options: ArticleStateType) => void;
+   }
+
+export const ArticleParamsForm: React.FC<ArticleParamsFormProps> = ({ setStyles }) => {
+
+    const [isOpen, setIsOpen] = useState(false);
+    const [options, setOptions] = useState({
+        fontFamilyOption: defaultArticleState.fontFamilyOption,
+        fontSizeOption: defaultArticleState.fontSizeOption,
+        fontColor: defaultArticleState.fontColor,
+        backgroundColor: defaultArticleState.backgroundColor,
+        contentWidth: defaultArticleState.contentWidth,
+      });
+      const handleInputChanges = (key: keyof ArticleStateType, value: OptionType) => {
+        setOptions((prevOptions) => ({
+          ...prevOptions,
+          [key]: value,
+        }));
+      };
+
+    const classNameContainer = clsx({
+        [styles.container]: true,
+        [styles.container_open]: isOpen,
+    });
+
+    const handleSubmit:React.FormEventHandler<HTMLFormElement> = (e) =>{
+        e.preventDefault();
+        setStyles(options)
+    }
+    const handleReset:React.FormEventHandler<HTMLFormElement> = (e) =>{
+        setStyles(defaultArticleState)
+        setOptions(defaultArticleState)
+    }
+
+    return (
+        <>
+            <ArrowButton open={isOpen} onClick={()=>{setIsOpen(!isOpen)}} />
+            <aside className={classNameContainer}>
+                <form onSubmit={handleSubmit} onReset={handleReset} className={styles.form}>
+                    <Text size={31} weight={800} uppercase={true}>Задайте параметры</Text>
+                    <Select onChange={(value) => handleInputChanges('fontFamilyOption', value)}  selected={options.fontFamilyOption} options={fontFamilyOptions} title={'Шрифт'}/>
+                    <RadioGroup onChange={(value) => handleInputChanges('fontSizeOption', value)} name={'fontSizeOption'} selected={options.fontSizeOption} options={fontSizeOptions}  title={'Размер шрифта'}/>
+                    <Select onChange={(value) => handleInputChanges('fontColor', value)} selected={options.fontColor} options={fontColors} title={'Цвет шрифта'}/>
+                    <Separator />
+                    <Select onChange={(value) => handleInputChanges('backgroundColor', value)}  selected={options.backgroundColor} options={backgroundColors} title={'Цвет фона'}/>
+                    <Select onChange={(value) => handleInputChanges('contentWidth', value)} selected={options.contentWidth} options={contentWidthArr} title={'Ширина контента'}/>
+                    <div className={styles.bottomContainer}>
+                        <Button title='Сбросить' type='reset' />
+                        <Button title='Применить' type='submit' />
+                    </div>
+                </form>
+            </aside>
+        </>
+    );
 };
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,10 @@
 import { createRoot } from 'react-dom/client';
-import { StrictMode, CSSProperties } from 'react';
+import { StrictMode, CSSProperties, useState } from 'react';
 import clsx from 'clsx';
 
 import { Article } from './components/article/Article';
 import { ArticleParamsForm } from './components/article-params-form/ArticleParamsForm';
-import { defaultArticleState } from './constants/articleProps';
+import { ArticleStateType, defaultArticleState, FontFamiliesClasses, OptionType } from './constants/articleProps';
 
 import './styles/index.scss';
 import styles from './styles/index.module.scss';
@@ -12,27 +12,32 @@ import styles from './styles/index.module.scss';
 const domNode = document.getElementById('root') as HTMLDivElement;
 const root = createRoot(domNode);
 
+
 const App = () => {
-	return (
-		<div
-			className={clsx(styles.main)}
-			style={
-				{
-					'--font-family': defaultArticleState.fontFamilyOption.value,
-					'--font-size': defaultArticleState.fontSizeOption.value,
-					'--font-color': defaultArticleState.fontColor.value,
-					'--container-width': defaultArticleState.contentWidth.value,
-					'--bg-color': defaultArticleState.backgroundColor.value,
-				} as CSSProperties
-			}>
-			<ArticleParamsForm />
-			<Article />
-		</div>
-	);
+    const [primeOptions, setPrimeOptions] = useState(defaultArticleState)
+
+    const setStyles = (options: ArticleStateType ) => {
+        setPrimeOptions(options)
+    };
+
+    return (
+        <div
+        className={clsx(styles.main)}
+        style={{
+            '--font-family': primeOptions.fontFamilyOption.value,
+            '--font-size': primeOptions.fontSizeOption.value,
+            '--font-color': primeOptions.fontColor.value,
+            '--container-width': primeOptions.contentWidth.value,
+            '--bg-color': primeOptions.backgroundColor.value,
+        } as CSSProperties}>
+            <ArticleParamsForm  setStyles={setStyles}  />
+            <Article />
+        </div>
+    );
 };
 
 root.render(
-	<StrictMode>
-		<App />
-	</StrictMode>
+    <StrictMode>
+        <App />
+    </StrictMode>
 );
